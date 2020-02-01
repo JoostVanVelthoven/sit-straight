43000624c9c11ff373153a139eed9e7d10bc6789
Joost <joost@Joosts-iMac.home>
2020-02-01 16:04:22 +0100

    face.js test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,66 +1,65 @@
-import React, { useEffect  , useRef} from "react"
+import React, { useEffect, useRef } from "react"
 import * as faceapi from 'face-api.js';
 import './index.css';
 
-export default () =>  { 
+export default () => {
 
-   
     const cameraRef = useRef(null);
 
-    useEffect( () => {
+    async function loadCamera() {
+        await Promise.all([
+            faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+            faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+            faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+            faceapi.nets.faceExpressionNet.loadFromUri('/models')
+          ]);
 
-     
-        // await facepi.nets.tinyFaceDetector.loadFromUri();
-        
+        cameraRef.current.addEventListener('play', () => {
 
-        // cameraRef.current.addEventListener('play', () => {
-            
-        //     const video = cameraRef.current;
-            
-        //     const canvas = faceapi.createCanvasFromMedia(cameraRef.current)
-        //     document.body.append(canvas)
-        //     const displaySize = { width: video.width, height: video.height }
-        //     faceapi.matchDimensions(canvas, displaySize)
-        //     setInterval(async () => {
-        //       const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-        //       const resizedDetections = faceapi.resizeResults(detections, displaySize)
-        //       canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        //       //faceapi.draw.drawDetections(canvas, resizedDetections)
-        //       //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        //       //faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-        //     }, 100)
-        //   })
-        
-        
-       
+            const video = cameraRef.current;
+
+            const canvas = faceapi.createCanvasFromMedia(cameraRef.current)
+            document.body.append(canvas)
+            const displaySize = { width: video.width, height: video.height }
+            faceapi.matchDimensions(canvas, displaySize);
 
 
-        //window.setTimeout(function() {
+            console.log(displaySize)
 
+            setInterval(async () => {
+              const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+              console.log(detections);
+              const resizedDetections = faceapi.resizeResults(detections, displaySize)
+              canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+              faceapi.draw.drawDetections(canvas, resizedDetections)
+              faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+              faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+            }, 100)
+        });
 
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: false
+        }
+        ).then((stream) => {
 
-            navigator.mediaDevices.getUserMedia({
-                video: true,
-                audio: false
-            }
-                ).then((stream)=>{
+            cameraRef.current.srcObject = stream;
+        })
 
-                    cameraRef.current.srcObject = stream;  
-                 })              
-   
-      //},1000);
+    }
+
+    useEffect(() => {
+        loadCamera();      
     });
-  
 
 
-    return(<html>
 
-<body>
-    <header>
-        Sit straigt coach - sit straigt and move the slider  😁
+
+    return (<>
+        <header>
+            Sit straigt coach - sit straigt and move the slider  😁
     </header>
-    <main>
-    <video id="video" ref={cameraRef} width="320" height="240" preload="preload" autoPlay="autoPlay" loop="loop" muted="muted"></video>
-      <canvas id="canvas" width="320" height="240"></canvas></main></body>
-
-</html>); }
+        <main>
+            <video id="video" ref={cameraRef} width="320" height="240" preload="preload" autoPlay="autoPlay" loop="loop" muted="muted"></video>
+            <canvas id="canvas" width="320" height="240"></canvas></main></>);
+}
