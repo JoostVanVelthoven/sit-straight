24fdcbe04a35488f73ca3d25931c68af13fd10cb
Joost <joost@Joosts-iMac.lan>
2020-02-16 08:14:53 +0100

    test pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,16 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [`gatsby-plugin-offline`, {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    }
+  ]
 }
