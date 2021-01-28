1268ca56807864ba5ea45976f4b8fe72016fe18f
JoostVanVelthoven <vanvelthoven@gmail.com>
2021-01-28 18:57:46 +0100

    Update footerComponent.js

--- src/components/footerComponent.js
+++ src/components/footerComponent.js
@@ -4,7 +4,7 @@ import React  from "react"
 export default () =>  {  
     return (<footer>
             <div>Do you have problems with your posture? This website helps you with the camera and machine learining to sit straight. </div>
-            <div><a href="https://github.com/JoostVanVelthoven/sit-straight" target="_blank" rel="noopener noreferrer">Fork me on Github</a>
+            <div><a href="https://github.com/JoostVanVelthoven/sit-straight" target="_blank" rel="noopener noreferrer">Fork me on Github</a></div>
     </footer>);
     }
     
